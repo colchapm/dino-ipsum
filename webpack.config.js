bcbb983af9a22b6ace104b460f047011830e5880
Collin Chapman <cchap14@gmail.com>
2021-03-16 20:39:55 -0700

    create environment
    Marni Sucher <suchermarni@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Document',
+      title: 'Dino Ipsum',
       template: './src/index.html',
       inject: 'body'
     })
