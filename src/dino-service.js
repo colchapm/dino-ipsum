f208e795acf18cb5bff4efa92815ff81c8b1af8c
Collin Chapman <cchap14@gmail.com>
2021-03-17 19:39:49 -0700

    call api successfully and output text in DOM
    Juan Hasbun <zemenarwow@gmail.com>

--- src/dino-service.js
+++ src/dino-service.js
@@ -1,8 +1,9 @@
 export default class Dino {
   static getDino(par, words) {
+    console.log("in getDino");
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `http://dinoipsum.herokuapp.com/api/?format=html&words=1&paragraphs=1`;
+      const url = `http://dinoipsum.herokuapp.com/api/?format=json&words=2&paragraphs=2`;
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
@@ -15,16 +16,4 @@ export default class Dino {
     });
   }
 }
-
-// var xhr = new XMLHttpRequest();
-
-// xhr.open('GET', 'http://dinoipsum.herokuapp.com/api/?format=html&words=10&paragraphs=3');
-// xhr.onload = function() {
-//   if (this.status === 200) {
-//     document.querySelector('#some-awesome-container').innerHTML = this.response;
-//   } else {
-//     console.log('Where did all the dinosaurs go?');
-//   }
-// };
-// xhr.send();
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Dino from './dino-service.js';
+
+$(document).ready(function() {
+  $('#getDinos').click(function() {
+    // let words = $('#wordInput').val();
+    // let paragraphs = $('#paragraphInput').val();
+    let par = parseInt(1);
+    let words = parseInt(1);
+
+    let promise = Dino.getDino(par, words);
+    promise.then(function(response) {
+      console.log("in promise.then");
+      const body = JSON.parse(response);
+      $('.output').text(`${body[1][1]}`);
+      $('.showErrors').text("");
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+      $('.output').text("");
+    });
+  });
+});
